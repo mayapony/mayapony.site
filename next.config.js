8e8d23b6d9a88b20bb02b5f334837ecc81fe7ace
mayapony <mayapony@foxmail.com>
2025-05-12 02:13:44 +0800

    🚀 尝试修改部署配置

--- next.config.js
+++ next.config.js
@@ -2,6 +2,11 @@
 const nextConfig = {
   reactStrictMode: true,
   output: "export",
+  basePath: "/mayapony",
+  assetPrefix: "/mayapony",
+  images: {
+    unoptimized: true, // ← export 模式下禁用图片优化
+  },
 };
 
 module.exports = nextConfig;
